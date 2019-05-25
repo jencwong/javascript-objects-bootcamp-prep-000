c1d9b571862f74ac4fbd3c8aa10f3e9aaa08eb83
Jen W <jsc525@gmail.com>
2019-05-25 03:52:15 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,6 @@ var playlist = {artistName: "songTitle"};
 
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {['Phil Ochs']: });
+  return Object.assign({}, playlist, {['Phil Ochs']: "Here's to the State of Mississippi"});
 }
 
-updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
